f6ddbf24bf1fb40c4ce73418212502e82e22dc18
ubt.zhang <zigzag.zhang@ubtrobot.com>
2019-04-09 20:55:16 +0800

    change main js

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 const { app, BrowserWindow } = require('electron')
 
+let win
+
 const createWindow = () => {
-    let win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600
     })
-
     win.loadFile('app/index.html')
+    
+    // 开发者调试工具
+    win.webContents.openDevTools()
+
+    // 快捷图标上的进度条
+    win.setProgressBar(0.5)
+
+    win.on('close', ()=>{
+        win = null
+    })
 }
 
 app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+      createWindow()
+    }
+})
+
+app.addRecentDocument('/Users/ubt/Desktop/work.type')
